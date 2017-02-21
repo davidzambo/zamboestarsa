ef054a57e10ea482805b2ee505b8cad740f93cbe
Zámbó Sándor Dávid <david.a.zambo@gmail.com>
2017-02-21 22:52:02 +0100

    HTML text ready, testing CSS

---
+++ zamboestarsa/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Üdvözöljük a Zámbó és társa bt. honlapján</title>
+  <link type="text/css" rel="stylesheet" href="styles.css" />
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+</head>
+<body>
+<div class="container">
+<div class="nav-bar">
+  <ul>
+    <li><a href="#welcome">Köszöntő</a></li>
+    <li><a href="#profile">Miben segíthetünk</a></li>
+    <li><a href="#gallery">Referenciáink</a></li>
+    <li><a href="#contacts">Elérhetőségeink</a></li>
+  </ul>
+</div>
+<div id="welcome">
+  <h1>Köszöntöm honlapunkon!</h1>
+  <p>Engedje meg, hogy bemutatkozzam és bemutassam cégünket!</p>
+  <p>Nevem Zámbó Sándor, a Soproni Faipari Egyetemen végzett szakember vagyok.</p>
+  <p>Cégünk 1993-ban alakult.</p>
+  <p>A jól képzett szakember gárda, a folyamatos fejlesztések eredménye, valamint a gépesítettségünk teszi lehetővé a jó minőségű termékek előállítását.</p>
+  <p>Elsősorban egyedi lakás-, iroda, és iskolabútorok, valamint székek, nyílászárók, falépcsők gyártására vagyunk felkészülve, de nagyon kedvező áron (gyártói ár alatt) forgalmazunk irodai, munkahelyi, iskolai, kollégiumi bútorokat, foteleket, székeket, kanapékat, ágyakat, padokat és utcabútorokat.</p>
+  <p>Termékeinket folyamatosan jó minőségben, gyosan, meghatározott határidőn belül és reális áron állítjuk elő, illetve forgalmazzuk.</p>
+  <p>Újdonság, amit csak cégünk vállal: 2 évig karbantartjuk termékeinket!</p>
+  <p>Ennek köszönhető töretlen fejlődésünk, melyet a 2010-es év során a Borsod-Abaúj-Zemplén megyei Kereskedelmi és Iparkamara Oklevéllel is elismert.</p>
+  <p>Kérem tekintse meg referenciáinkat, s amennyiben kérdése merül fel, keressen minket bizalommal!</p>
+
+  <p>Tisztelettel a sikeres együttműködés reményében:</p>
+
+  <p>Zámbó Sándor</p>
+  <p>üzletvezető</p>
+</div>
+<div id="profile">
+  <p>Bútoraink:<br />
+  Iroda és iskola berendezéseink, lakásbútoraink, gyerekbútoraink, igazodnak az elvárt igényekhez, az anyagok gazdag választékával, szabványosított méretekkel dolgozunk, így a megye egyik legkedvezőtt árfekvésű szállítói vagyunk.</p>
+  <p>Ajtóink, ablakaink:<br />
+  2008-tól számítógép vezérlésű géppel állítjuk elő hossz toldott, rétegragasztott borovi fenyőfából, roto vasalattal, fém vízvetővel szerelve. Gyártani tudjuk felületkezeléssel 3 rétegű üvegezéssel, dupla gumiszigeteléssel. Beépítjük a terméket. 20 év garanciát vállalunk!</p>
+  <p>Garázskapu szekcionált, motoros, távirányítású:<br />
+  Újdonságát az adja, hogy rétegragasztott borovi fenyőfából készítjük.</p>
+  <p>Falépcsőink:<br />
+  Helyszíni felmérés alapján tölgyfa, bükkfa, fenyőfa anyagokból gyártjuk, de lehetőség van más fafajok beépítésére is.</p>
+  <p>A gyors helyszíni beszerelés garantált, igyekszünk a megrendelőnek a lehető legkisebb kellemetlenséget okozni.
+  Ennek köszönhető töretlen fejlődésünk.</p>
+  <p>Üzletünk:<br />
+  Bűtor- és épületasztalos vasalatokat, szerelvényeket, kilincseket, zárakat, díszítő elemeket, díszüvegeket, rész és króm köszöbborításokat, lépcsőkorlát elemeket forgalmazunk nagy- és kiskereskedelmi áron.<br />
+  Miskolc és környékének lakosságán kívül néhány cég, mely minket választott beszállítójának:</p>
+  <ul>
+    <li>MOL</li>
+    <li>Miskolci Egyetem,</li>
+    <li>Környezetvédelmi Főfelügyelőség Borsod-Abaúj-Zemplén megyei kirendeltsége</li>
+    <li>Borsod-Abaúj-Zemplén megyei munkaügyi központ</li>
+    <li>Felsőzsolcai ÁMK</li>
+    <li>Borsod-Abaúj-Zemplén megyei ügyészség</li>
+    <li>Szerencsejáték Zrt.</li>
+  </ul>
+  <p>Megrendelésének kifizetésekor lehetőség van készpénzes számlás, átutalásos számlás vagy részletfizetéses rendezésre.</p>
+  <p>Amennyiben van lehetősége és igénye ilyen jellegű munkára, megrendelésével tegyen próbára bennünket, vagy válasszon ismét bennünket!</p>
+</div>
+<div id="contacts">
+  <h2>Zámbó és Társa Bt.<br />
+  Bútor és épületasztalos gyártó és kereskedő cég</h2>
+  <h3>Iroda és üzem:</h3>
+  H-3561 Felsőzsolca, Szent István út 5.<br />
+  Tel./Fax: +36 (46) 384 509<br />
+  Mobil: + 36 (30) 9 288 808<br />
+  E-Mail: info@zamboestarsa.hu<br />
+  Web: www.zamboestarsa.hu</p>
+  <div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2664.5824995573103!2d20.85313295083752!3d48.09899797911839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473f5f8024bb7a51%3A0x34eccbd29c343d9c!2sFels%C5%91zsolca%2C+Szent+Istv%C3%A1n+u.+5%2C+3561!5e0!3m2!1sen!2shu!4v1486978840701" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+
+  <h4>Bemutató üzlet és szerelvénybolt:</h4>
+  <p><strong>FAVILÁG</strong><br />
+  H-3525 Miskolc, Szentpáli út 1.<br />
+  Tel.: +36 (46) 756 660<br />
+  <div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2664.3698926128177!2d20.786695550837784!3d48.103099879118744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47409ffbdbc5da4d%3A0x66a6cf7afed9f690!2sMiskolc%2C+Szentp%C3%A1li+u.+1%2C+3525!5e0!3m2!1sen!2shu!4v1486978469058" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+
+  <h4>Bemutató terem nyitvatartása:</h4>
+  <table>
+    <tr>
+      <td>Hétfő</td>
+      <td>8:30 - 17:00</td>
+    </tr>
+    <tr>
+      <td>Kedd</td>
+      <td>8:30 - 17:00</td>
+    </tr>
+    <tr>
+      <td>Szerda</td>
+      <td>8:30 - 17:00</td>
+    </tr>
+    <tr>
+      <td>Csütörtök</td>
+      <td>8:30 - 17:00</td>
+    </tr>
+    <tr>
+      <td>Péntek</td>
+      <td>8:30 - 17:00</td>
+    </tr>
+    <tr>
+      <td>Szombat</td>
+      <td>9:00 - 12:00</td>
+    </tr>
+    <tr>
+      <td>Vasárnap</td>
+      <td>zárva</td>
+    </tr>
+  </table>
+</div>
+</div>
+</body>
+</html>
